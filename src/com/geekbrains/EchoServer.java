a4a67ad2ce2861c47fa989b7fd5e353228ca34b4
South <south123@mail.ru>
2022-04-08 17:47:56 +0300

    made server

--- src/com/geekbrains/EchoServer.java
+++ src/com/geekbrains/EchoServer.java
@@ -1,21 +1,52 @@
 package com.geekbrains;
 
 import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class EchoServer {
+    DataInputStream in;
+    DataOutputStream out;
 
-    public void openConnect() {
+    public static void main(String[] args) {
+        String message;
+        EchoServer server = new EchoServer();
+        server.startServer();
+        while (true) {
+            try {
+                message = server.receiveMessage();
+                if ("/end".equalsIgnoreCase(message))
+                    break;
+                System.out.println(message);
+                server.sendMessage(message);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private String receiveMessage() throws IOException {
+        return in.readUTF();
+    }
+
+    private void sendMessage(String message) throws IOException {
+        out.writeUTF(message);
+    }
+
+    private void startServer() {
         Socket socket;
 
         try (ServerSocket serverSocket = new ServerSocket(8189)) {
             System.out.println("Сервер запущен. Ожидание подключения....");
             socket = serverSocket.accept();
             System.out.println("Клиент подключился");
-            DataInputStream in = new DataInputStream(socket.getInputStream());
-            
+
+            in = new DataInputStream(socket.getInputStream());
+
+            out = new DataOutputStream(socket.getOutputStream());
+
         } catch (IOException e) {
             e.printStackTrace();
         }
